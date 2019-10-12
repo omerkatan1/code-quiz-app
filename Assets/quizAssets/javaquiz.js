3b82c0d5db0dd17700a3af2d5984bf491178d7ec
Omer Katan <omerkatan@live.com>
2019-10-11 22:18:37 -0700

    started working on timer

--- Assets/quizAssets/javaquiz.js
+++ Assets/quizAssets/javaquiz.js
@@ -1,5 +1,14 @@
+var secDisplay = document.querySelector("#secDisplay");
 
-
+var seconds = 75;
 function startTimer() {
-    
+    setInterval(function() {
+        seconds--;
+        console.log(seconds);
+
+
+        seconds = seconds + '';
+
+        secDisplay.appendChild(seconds);
+    }, 1000)
 }
