d79021ece5b8d8a6ffa1f6085b5aa86e809c2b69
Omer Katan <omerkatan@live.com>
2019-10-11 21:56:30 -0700

    added some start button function

--- Assets/quizAssets/javaquiz.js
+++ Assets/quizAssets/javaquiz.js
@@ -1,10 +1,5 @@
 
 
-
-
-
-
-$('document').ready(function() {
-    var startButton = document.createElement("<button>");
-    $("#mainBody-quiz").append(startButton);
-});
+function startTimer() {
+    
+}
